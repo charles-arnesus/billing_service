f4dbcc43f4013e1e7e62633076be72f2c73e54c2
Charles Arnesus <CharlesArnesus.Matulessy@mitrais.com>
2020-09-02 14:12:03 +0700

    [Charles] add inquiry controller and models

--- src/main/java/com/charles/billing_service/controllers/BillingServiceController.java
+++ src/main/java/com/charles/billing_service/controllers/BillingServiceController.java
@@ -2,19 +2,22 @@ package com.charles.billing_service.controllers;
 
 import com.charles.billing_service.models.Biller;
 import com.charles.billing_service.models.Bills;
+import com.charles.billing_service.models.InquiryRequest;
+import com.charles.billing_service.models.InquiryResponse;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api")
 public class BillingServiceController {
 
+    public static final String PDAM = "pdam";
+    public static final String POSTPAID = "postpaid";
     @Autowired
     private RestTemplate restTemplate;
 
@@ -26,10 +29,10 @@ public class BillingServiceController {
             @ApiParam(value = "ID value for the biller that need to be retrieved", required = true)
             @PathVariable("billerId")
                     String billerId) {
-        return new Biller("pdam_bandung", "PDAM Tirtawening", "pdam");
+        return new Biller("halo", "Halo", "postpaid");
     }
 
-    @GetMapping("history/{userId}")
+    @GetMapping("/history/{userId}")
     @ApiOperation(value = "Retrieve all payment history by userId",
             notes = "Provide a userId to retrieve all payment history",
             response = Bills.class)
@@ -38,5 +41,32 @@ public class BillingServiceController {
             @PathVariable("userId")
                     String userId) {
         return restTemplate.getForObject("http://billing-history-service/api/history/" + userId, Bills.class);
+    }
+
+    @PostMapping("/inquiry")
+    public InquiryResponse doInquiry(@RequestBody InquiryRequest inquiryRequest) {
+        final String billerId = inquiryRequest.getBillerId();
+        final Biller biller = getBiller(billerId);
+        final String billerCategory = biller.getCategory();
+        InquiryResponse response = new InquiryResponse();
+        switch (billerCategory) {
+            case PDAM:
+                response = restTemplate.postForObject(
+                        "http://billing-pdam-client/api/inquiry/",
+                        inquiryRequest,
+                        InquiryResponse.class);
+            break;
+            case POSTPAID:
+                response = restTemplate.postForObject(
+                        "http://billing-postpaid-client/api/inquiry/",
+                        inquiryRequest,
+                        InquiryResponse.class);
+            break;
+            default:
+                break;
+        }
+        response.setInquiryId(UUID.randomUUID());
+
+        return response;
     }
 }
