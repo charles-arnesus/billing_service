be6a5fcdfc0ae08347d75dfe293c37c8ffa4f88c
Charles Arnesus <CharlesArnesus.Matulessy@mitrais.com>
2020-09-02 08:19:46 +0700

    [Charles] enable swagger

--- src/main/java/com/charles/billing_service/controllers/BillingServiceController.java
+++ src/main/java/com/charles/billing_service/controllers/BillingServiceController.java
@@ -1,21 +1,17 @@
 package com.charles.billing_service.controllers;
 
 import com.charles.billing_service.models.Biller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collections;
-import java.util.List;
-
 @RestController
-@RequestMapping("/biller")
+@RequestMapping("/api/biller")
 public class BillingServiceController {
 
-    @RequestMapping("/{billerId}")
-    public List<Biller> getBiller(@PathVariable("billerId") String billerId){
-        return Collections.singletonList(
-                new Biller("pdam_bandung", "PDAM Tirtawening", "pdam")
-        );
+    @GetMapping("/{billerId}")
+    public Biller getBiller(@PathVariable("billerId") String billerId){
+        return new Biller("pdam_bandung", "PDAM Tirtawening", "pdam");
     }
 }
