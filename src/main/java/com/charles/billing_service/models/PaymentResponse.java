844d9547be677f1968e8cc0d027e82e9236835a1
Charles Arnesus <CharlesArnesus.Matulessy@mitrais.com>
2020-09-02 23:09:20 +0700

    [Charles] add paymentDate to payment response

--- src/main/java/com/charles/billing_service/models/PaymentResponse.java
+++ src/main/java/com/charles/billing_service/models/PaymentResponse.java
@@ -1,5 +1,6 @@
 package com.charles.billing_service.models;
 
+import java.time.LocalDate;
 import java.util.UUID;
 
 public class PaymentResponse {
@@ -8,17 +9,25 @@ public class PaymentResponse {
     private Double totalAmount;
     private String status;
     private String customerAccountId;
+    private LocalDate paymentDate;
 
     public PaymentResponse() {
 
     }
 
-    public PaymentResponse(UUID paymentId, String billerId, Double totalAmount, String status, String customerAccountId) {
+    public PaymentResponse(
+            UUID paymentId,
+            String billerId,
+            Double totalAmount,
+            String status,
+            String customerAccountId,
+            LocalDate paymentDate) {
         this.paymentId = paymentId;
         this.billerId = billerId;
         this.totalAmount = totalAmount;
         this.status = status;
         this.customerAccountId = customerAccountId;
+        this.paymentDate = paymentDate;
     }
 
     public String getBillerId() {
@@ -59,5 +68,13 @@ public class PaymentResponse {
 
     public void setCustomerAccountId(String customerAccountId) {
         this.customerAccountId = customerAccountId;
+    }
+
+    public LocalDate getPaymentDate() {
+        return paymentDate;
+    }
+
+    public void setPaymentDate(LocalDate paymentDate) {
+        this.paymentDate = paymentDate;
     }
 }
