639806e2bb72bcdf7d8bf2fdaa59b82003a62e27
Charles Arnesus <CharlesArnesus.Matulessy@mitrais.com>
2020-09-03 19:05:39 +0700

    [Charles] add migration to create biller table

--- src/main/resources/db/migration/V1__insert_biller_data.sql
+++ src/main/resources/db/migration/V1__insert_biller_data.sql
@@ -1,3 +1,11 @@
+CREATE TABLE biller (
+  biller_id VARCHAR(50),
+  biller_name VARCHAR(50),
+  category VARCHAR(50)
+);
+
+ALTER TABLE biller ADD CONSTRAINT biller_pk PRIMARY KEY (biller_id);
+
 INSERT INTO biller(
 	biller_id, biller_name, category)
 	VALUES ('halo', 'Halo Postpaid', 'postpaid'),
